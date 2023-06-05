cd3198fcd6d60209e9332deb9b147820b1cebc10
Thuan <vanthuan30102k@gmail.com>
2023-06-05 17:58:32 +0700

    popup

---
+++ HTCMage/RecommendPopup/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Checkout/js/view/minicart': {
+                'HTCMage_RecommendPopup/js/minicart': true
+            }
+        }
+    }
+};
