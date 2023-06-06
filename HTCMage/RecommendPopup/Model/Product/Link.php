6b550e03fe2c46bfbb986d11918305c56b875afa
Thuan <vanthuan30102k@gmail.com>
2023-06-06 17:07:19 +0700

    admin config suggest product

---
+++ HTCMage/RecommendPopup/Model/Product/Link.php
@@ -0,0 +1,6 @@
+<?php
+namespace HTCMage\RecommendPopup\Model\Product;
+class Link extends \Magento\Catalog\Model\Product\Link
+{
+    const LINK_TYPE_CUSTOMLINKED = 17;
+}
--- HTCMage/RecommendPopup/view/frontend/requirejs-config.js
+++
@@ -1,9 +0,0 @@
-var config = {
-    config: {
-        mixins: {
-            'Magento_Checkout/js/view/minicart': {
-                'HTCMage_RecommendPopup/js/minicart': true
-            }
-        }
-    }
-};
